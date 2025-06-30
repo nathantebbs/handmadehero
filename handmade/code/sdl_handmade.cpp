2eb701c330eec4faaa75724ccecd9e0ba2ac314e
nathantebbs <nathan.tebbs2004@gmail.com>
2025-06-29 23:10:49 -0700

    feat: Renderer
    - removed old screenSurface (use for images or more static things
    instead)

--- handmade/code/sdl_handmade.cpp
+++ handmade/code/sdl_handmade.cpp
@@ -4,9 +4,12 @@
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_log.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL_oldnames.h>
+#include <SDL3/SDL_render.h>
 #include <SDL3/SDL_stdinc.h>
 #include <SDL3/SDL_surface.h>
 #include <SDL3/SDL_video.h>
+#include <cstdio>
 
 /* Constants */
 constexpr int screenWidth{600};
@@ -19,7 +22,7 @@ void close(); // Frees media + shutdown SDL
 
 /* Globals */
 SDL_Window *win{nullptr};
-SDL_Surface *screenSurface{nullptr};
+SDL_Renderer *ren{nullptr};
 
 /* Function Implementations */
 bool init() {
@@ -32,13 +35,19 @@ bool init() {
         success = false;
     } else {
         // Create Window
-        win = SDL_CreateWindow("HandmadeHero", screenWidth, screenHeight, 0);
+        win = SDL_CreateWindow("HandmadeHero", screenWidth, screenHeight,
+                               SDL_WINDOW_RESIZABLE);
         if (win == nullptr) {
             SDL_Log("ERROR: SDL Window could not be created! SDL Error: %s\n",
                     SDL_GetError());
             success = false;
         } else {
-            screenSurface = SDL_GetWindowSurface(win);
+            ren = SDL_CreateRenderer(win, "opengl");
+            if (ren == nullptr) {
+                SDL_Log(
+                    "ERROR: SDL Renderer could not be created! SDL ERROR: %s\n",
+                    SDL_GetError());
+            }
         }
     }
 
@@ -46,11 +55,13 @@ bool init() {
 }
 
 void close() {
+    // Destroy Renderer
+    SDL_DestroyRenderer(ren);
+    ren = nullptr;
 
     // Destroy Win
     SDL_DestroyWindow(win);
     win = nullptr;
-    screenSurface = nullptr;
 
     SDL_Quit();
 }
@@ -62,24 +73,29 @@ int main(int argc, char **argv) {
         SDL_Log("ERROR: Unable to initialize program!\n");
         status = 1;
     } else {
-        // TODO: Load media here?
-
         bool quit{false};
         SDL_Event e;
         SDL_zero(e);
 
         while (quit == false) {
             while (SDL_PollEvent(&e) == true) {
-                if (e.type == SDL_EVENT_QUIT) {
+                switch (e.type) {
+                case SDL_EVENT_QUIT:
                     quit = true;
+                    break;
+                case SDL_EVENT_WINDOW_RESIZED:
+                    printf("SDL_WINDOWEVENT_RESIZED (%d, %d)\n", e.window.data1,
+                           e.window.data2);
+                    // TODO: this isn't really flashing white the way I want
+                    // when the window is resized
+                    SDL_SetRenderDrawColor(ren, 0xFF, 0xFF, 0xFF, 0xFF);
+                    break;
                 }
             }
 
-            SDL_FillSurfaceRect(
-                screenSurface, nullptr,
-                SDL_MapSurfaceRGB(screenSurface, 0x18, 0x18, 0x18));
-
-            SDL_UpdateWindowSurface(win);
+            SDL_SetRenderDrawColor(ren, 0x18, 0x18, 0x18, 0x18);
+            SDL_RenderClear(ren);
+            SDL_RenderPresent(ren);
         }
     }
 
